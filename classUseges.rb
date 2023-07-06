b736ecb03fc7af60a84b51612da4eeac19a1b5e2
zanysploit <83715370+Zehrakmnl@users.noreply.github.com>
2023-07-06 18:03:39 +0300

    Update classUseges.rb

--- classUseges.rb
+++ classUseges.rb
@@ -1,20 +1,28 @@
+#!/usr/bin/env ruby
 
 class Lobi
-    def customer (name = "Harleen Quinzel")
-        @name = name
+    attr_accessor :name
+
+    def initialize(name = "Harleen Quinzel") # WARNING dont change the 'initialize'. when u change method will not work.
+      @name = name
     end        
+
     def enter_customer
-      puts "Hello #{@name} welcome to Turkey"
-    end    
+      puts "Hello #{@name}, welcome to Turkey"
+    end
+
     def exit_customer
       puts "Bye #{@name}, see you again "
     end    
 end
 
 
+
 if __FILE__ == $0
     lobi = Lobi.new
-    lobi.enter_customer 
+    lobi.enter_customer
 
+    lobi.name = "Romy"
+    lobi.enter_customer
 
 end
