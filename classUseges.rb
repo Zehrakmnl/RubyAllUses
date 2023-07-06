5b0fdf4618882f63ebd995e108ce0c111c1be7eb
zanysploit <83715370+Zehrakmnl@users.noreply.github.com>
2023-07-06 17:37:26 +0300

    Update classUseges.rb

--- classUseges.rb
+++ classUseges.rb
@@ -1,1 +1,20 @@
 
+class Lobi
+    def customer (name = "Harleen Quinzel")
+        @name = name
+    end        
+    def enter_customer
+      puts "Hello #{@name} welcome to Turkey"
+    end    
+    def exit_customer
+      puts "Bye #{@name}, see you again "
+    end    
+end
+
+
+if __FILE__ == $0
+    lobi = Lobi.new
+    lobi.enter_customer 
+
+
+end
